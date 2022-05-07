105dcf4570a1884b6e0c24de25dfd2cd5cdfc7b1
David Cândido <103040608+DavidHelmer@users.noreply.github.com>
2022-05-07 18:28:03 -0300

    Update relatorio.sql

--- pset2/relatorio.sql
+++ pset2/relatorio.sql
@@ -1,1 +1,13 @@
+-- 1 -> Fazer a média salarial dos funcionários para cada departamento
+SELECT AVG(f.salario) AS media_salarial, d.nome_departamento
+FROM funcionario f
+INNER JOIN departamento d
+ON f.numero_departamento = d.numero_departamento
+GROUP BY d.nome_departamento;
 
+-- 2 -> Mostrar a media salárial dos homens e das mulheres
+SELECT AVG(f.salario) AS media_salarial, f.sexo
+FROM funcionario f
+GROUP BY f.sexo;
+
+-- 3 -> 
